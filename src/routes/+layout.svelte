5a30a2f29dbed88c9eeb73a454f4de2351a72bef
Ray1457 <rishitnarang222@gmail.com>
2025-09-20 04:48:17 +0530

    sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 	import bg from '$lib/assets/bg2.png';
-	import { Navbar } from '$lib';
+	import { Navbar, Sidebar } from '$lib';
 	import '../app.css';
 
 	let { children } = $props();
@@ -11,12 +11,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<main class="relative container h-screen w-screen bg-cover bg-center bg-fixed pl-72" style="background-image: url({bg});">
+<main class="relative min-h-screen w-full bg-cover bg-center bg-fixed lg:pl-32 lg:pr-80 text-white" style="background-image: url({bg});">
 	<!-- Background opacity overlay -->
-	<div class="absolute inset-0 bg-black/25 backdrop-blur-[2px] pointer-events-none"></div>
+	<div class="absolute inset-0 bg-black/20 backdrop-blur-[1px] pointer-events-none"></div>
 	
 	<Navbar />
-	<div class="relative z-10 h-full p-8">
+	<Sidebar />
+	<div class="relative h-full w-full p-8 lg:p-8 pt-24 lg:pt-8">
 		{@render children?.()}
 	</div>
+
+
+	
+	<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </main>
+
+
